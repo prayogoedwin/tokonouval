f19a85e4c0a08480f5080e91207d018f3156a30c
AlfaNgopi <aiprstv4869@gmail.com>
2026-05-04 15:43:37 +0700

    db setup Kategori, Satuan, Produk

---
+++ app/Models/Produk.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Produk extends Model
+{
+    //
+    // Schema::create('produks', function (Blueprint $table) {
+    //         $table->id();
+    //         $table->foreignId('toko_id')->constrained('tokos');
+    //         $table->foreignId('kategori_id')->constrained('kategories');
+    //         $table->string('name');
+    //         $table->integer('harga_beli');
+    //         $table->integer('harga_jual');
+    //         $table->timestamps();
+    //         $table->softDeletes();
+    //         $table->foreignId('created_by')->nullable();
+    //         $table->foreignId('updated_by')->nullable();
+    //         $table->foreignId('deleted_by')->nullable();
+    //     });
+
+    protected $fillable = [
+        'toko_id',
+        'kategori_id',
+        'name',
+        'harga_beli',
+        'harga_jual',
+        'created_by',
+        'updated_by',
+        'deleted_by',
+        
+    ];
+
+    public function kategori(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Kategori::class);
+    }
+    
+    public function toko(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Toko::class);
+    }
+}
